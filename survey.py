5f906f8e004d829fbf36ece8718a844099fa8b89
Dreamy <dreamy26@icloud.com>
2023-01-21 23:48:54 -0500

    build: Adds question array

---
+++ survey.py
@@ -0,0 +1,8 @@
+# Testing a class
+"""Collect anonymous answers to a survey question."""
+
+def __init__(self, question):
+    """Store a question, and prepare to store responses."""
+    self.question = question
+    self.responses = []
+    
