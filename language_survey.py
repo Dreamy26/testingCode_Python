4cdf3ad01c016c49bd37a5fa0a1fbaef66970a45
Dreamy <dreamy26@icloud.com>
2023-01-22 00:58:59 -0500

    build: Creates the first question for survey

---
+++ language_survey.py
@@ -0,0 +1,8 @@
+# Show that AnonymousSurvey works
+from survey import AnonymousSurvey
+
+# Define a question, and make a survey.
+question = "What language did you first learn to speak?"
+my_survey = AnonymousSurvey(question)
+
+
