4430c14ff087c1162b766462a630de456b34da01
Dreamy <dreamy26@icloud.com>
2023-01-22 00:07:33 -0500

    chore: Adds some code comments

--- survey.py
+++ survey.py
@@ -4,16 +4,17 @@
 def __init__(self, question):
     """Store a question, and prepare to store responses."""
     self.question = question
-    self.responses = []
-    
-def show_question(self):
+    self.responses = [] # empty list, to store responses   
+
+def show_question(self): # display survey question
     """Show the survey question"""
     print (self.question)
 
-def store_response(self, new_response):
+def store_response(self, new_response): # store a new response, and print all 
     """Store a single response to the survey."""
     self.responses.append()
-def show_results(self):
+
+def show_results(self): # show all 
     """Show all the responses that have been given."""
     print("Survey results:")
     for response in self.responses:
